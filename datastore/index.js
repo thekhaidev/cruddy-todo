aa7e9bc1f2a46ad33ea7442977b03f5f793fcc27
Khai Tran <kqtran917@yahoo.com>
2021-10-12 13:27:51 -0500

    Complete retrieving all todos

--- datastore/index.js
+++ datastore/index.js
@@ -12,31 +12,47 @@ exports.create = (text, callback) => {
   // items[id] = text;
   // callback(null, { id, text });
 
-// get the unique id
-// write the text with the filename of the unique id
-// if error callback(err)
-// else
-// items[id] = text;
-// callback(null, { id, text });
+  // get the unique id
+  // write the text with the filename of the unique id
+  // if error callback(err)
+  // else
+  // items[id] = text;
+  // callback(null, { id, text });
 
-counter.getNextUniqueId(getIDCB = (err, id) => {
- fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
-   if (err) {
-     callback(err)
-   } else{
-     items[id] = text;
-     callback(null, { id, text });
-   }
- })
-})
+  counter.getNextUniqueId(getIDCB = (err, id) => {
+    fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+      if (err) {
+        callback(err);
+      } else {
+        items[id] = text;
+        callback(null, { id, text });
+      }
+    });
+  });
 
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
-  });
-  callback(null, data);
+  // var data = _.map(items, (text, id) => {
+  //   return { id, text };
+  // });
+  // callback(null, data);
+
+  // read the data
+  // if error
+  // throw error
+  // map the data
+
+  fs.readdir(exports.dataDir, (err, data) => {
+    if (err) {
+      console.log(err)
+      callback(err);
+    } else {
+      var data = _.map(data, (value) => ({id: value.slice(0, 5), text: value.slice(0, 5)}));
+      callback(null, data);
+    }
+  })
+
 };
 
 exports.readOne = (id, callback) => {
