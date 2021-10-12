6e4d7e21247f157ef9d7719de8a302e365ec2606
Khai Tran <kqtran917@yahoo.com>
2021-10-12 15:00:15 -0500

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -72,13 +72,27 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
+
+  fs.access(`${exports.dataDir}/${id}.txt`, fs.F_OK, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, {id, text});
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
