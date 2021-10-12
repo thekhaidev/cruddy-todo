c596403940bbbddf543f93c62f64c8bf0300994e
Khai Tran <kqtran917@yahoo.com>
2021-10-12 15:03:09 -0500

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -96,14 +96,29 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+
+  fs.access(`${exports.dataDir}/${id}.txt`, fs.F_OK, (err) => {
+
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+      if (err) {
+        callback(err);
+      }
+      callback();
+    });
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
